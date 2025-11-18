14e4b4390938003c24bf6f56b8d756581e72488d
evanmwilliams <96541982+evanmwilliams@users.noreply.github.com>
2025-11-18 08:28:21 +0000

    Deploying to gh-pages from @ evanmwilliams/evanmwilliams.github.io@637d3b46b974e7ad7ce8ff5d0b16912428d0959f 🚀

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -407,44 +407,44 @@ ninja.data = [{
           section: "News",},{id: "news-selected-for-the-2025-nsf-computer-and-information-science-and-engineering-graduate-fellowship-and-mentoring-program-csgrad4us",
           title: 'Selected for the 2025 NSF Computer and Information Science and Engineering Graduate Fellowship...',
           description: "",
-          section: "News",},{id: "projects-project-1",
-          title: 'project 1',
-          description: "with background image",
+          section: "News",},{id: "projects-compiling-pytorch-models-to-calyx",
+          title: 'compiling pytorch models to calyx',
+          description: "an end-to-end compilation pipeline from pytorch to synthesizable hardware",
           section: "Projects",handler: () => {
               window.location.href = "/projects/1_project/";
-            },},{id: "projects-project-2",
-          title: 'project 2',
-          description: "a project with a background image and giscus comments",
+            },},{id: "projects-multi-core-risc-v-system",
+          title: 'multi-core risc-v system',
+          description: "a complete RTL-design of a RISC-V multicore system",
           section: "Projects",handler: () => {
               window.location.href = "/projects/2_project/";
-            },},{id: "projects-project-3-with-very-long-name",
-          title: 'project 3 with very long name',
-          description: "a project that redirects to another website",
+            },},{id: "projects-eta-compiler",
+          title: 'eta compiler',
+          description: "a fully-functional optimizing compiler targeting x86-64",
           section: "Projects",handler: () => {
               window.location.href = "/projects/3_project/";
-            },},{id: "projects-project-4",
-          title: 'project 4',
-          description: "another without an image",
+            },},{id: "projects-monte-carlo-based-options-pricing-with-high-level-synthesis",
+          title: 'monte carlo based options pricing with high-level synthesis',
+          description: "here we demonstrate the ability to accelerate monte carlo options pricing using HLS techniques",
           section: "Projects",handler: () => {
               window.location.href = "/projects/4_project/";
-            },},{id: "projects-project-5",
-          title: 'project 5',
-          description: "a project with a background image",
+            },},{id: "projects-computational-complexity-of-matrix-multiplication",
+          title: 'computational complexity of matrix multiplication',
+          description: "a survey of complexity theory for matrix multiplication",
           section: "Projects",handler: () => {
               window.location.href = "/projects/5_project/";
-            },},{id: "projects-project-6",
-          title: 'project 6',
-          description: "a project with no image",
+            },},{id: "projects-visualizing-the-mandelbrot-set",
+          title: 'visualizing the mandelbrot set',
+          description: "experiments with high performance computing and visualizations of the mandelbrot set",
           section: "Projects",handler: () => {
               window.location.href = "/projects/6_project/";
-            },},{id: "projects-project-7",
-          title: 'project 7',
-          description: "with background image",
+            },},{id: "projects-tetris-rl",
+          title: 'tetris rl',
+          description: "an implementation of an RL agent for tetris",
           section: "Projects",handler: () => {
               window.location.href = "/projects/7_project/";
-            },},{id: "projects-project-8",
-          title: 'project 8',
-          description: "an other project with a background image and giscus comments",
+            },},{id: "projects-risc-v-simulator-work-in-progress",
+          title: 'risc-v simulator (work in progress!)',
+          description: "a step towards a risc-v simulator written in modern cpp",
           section: "Projects",handler: () => {
               window.location.href = "/projects/8_project/";
             },},{id: "projects-project-9",
